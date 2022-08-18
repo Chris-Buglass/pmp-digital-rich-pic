cd7f82d1c23a9583d93aae71d6b8839389bd22e2
Chris-Buglass <chris.buglass@engineering.digital.dwp.gov.uk>
2022-08-18 12:08:24 +0100

    god

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+var http = require('http')
 var express = require('express');
 var app = express();
 var path = require('path');
-app.use(auth)
+var credentials = auth(req)
 if (!credentials || !check(credentials.name, credentials.pass)) {
   res.statusCode = 401
   res.setHeader('WWW-Authenticate', 'Basic realm="example"')
