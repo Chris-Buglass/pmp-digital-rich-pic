80d49386df80504e2c9861ed60e4a760346a4ccb
Chris-Buglass <chris.buglass@engineering.digital.dwp.gov.uk>
2022-08-18 11:54:31 +0100

    process.env.PORT

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ var path = require('path');
 app.use(express.static(__dirname)); // Current directory is root
 //app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
 
-app.listen(8000);
+app.listen(process.env.PORT || 5000);
 console.log('Listening on port 8000');
